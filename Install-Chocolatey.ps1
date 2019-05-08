54f6170df822330154d566fdb4668eb8ea4f910c
rapidliner0 <rapidliner.express+git@gmail.com>
2019-05-08 23:30:36 +0900

    Add Administrator authority check method

--- Install-Chocolatey.ps1
+++ Install-Chocolatey.ps1
@@ -1,1 +1,10 @@
+# 管理者権限チェック
+if (-not(([Security.Principal.WindowsPrincipal] `
+                [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+                [Security.Principal.WindowsBuiltInRole] "Administrator"`
+        ))) {
+    Write-Output "please execute as Administrator."
+    exit
+}
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
